fc0057430dd826db0d7bfc304bf5a0784d321e06
Allisson Nunes <allissonabn@gmail.com>
2022-10-16 21:57:28 -0300

    implementado validações de categoria

--- domain/src/main/java/com/github/allisson95/codeflix/domain/category/CategoryValidator.java
+++ domain/src/main/java/com/github/allisson95/codeflix/domain/category/CategoryValidator.java
@@ -6,6 +6,9 @@ import com.github.allisson95.codeflix.domain.validation.Validator;
 
 public class CategoryValidator extends Validator {
 
+    private static final int NAME_MIN_LENGTH = 3;
+    private static final int NAME_MAX_LENGTH = 255;
+
     private final Category category;
 
     public CategoryValidator(final Category aCategory, final ValidationHandler aHandler) {
@@ -15,8 +18,25 @@ public class CategoryValidator extends Validator {
 
     @Override
     public void validate() {
-        if (this.category.getName() == null) {
+        checkNameConstraints();
+    }
+
+    private void checkNameConstraints() {
+        final var name = this.category.getName();
+        if (name == null) {
             this.validationHandler().append(new Error("'name' should not be null"));
+            return;
+        }
+
+        if (name.isBlank()) {
+            this.validationHandler().append(new Error("'name' should not be empty"));
+            return;
+        }
+
+        final var length = name.trim().length();
+        if (length < NAME_MIN_LENGTH || length > NAME_MAX_LENGTH) {
+            this.validationHandler().append(new Error(
+                    "'name' must be between " + NAME_MIN_LENGTH + " and " + NAME_MAX_LENGTH + " characteres"));
         }
     }
 
--- domain/src/test/java/com/github/allisson95/codeflix/domain/category/CategoryTest.java
+++ domain/src/test/java/com/github/allisson95/codeflix/domain/category/CategoryTest.java
@@ -1,5 +1,6 @@
 package com.github.allisson95.codeflix.domain.category;
 
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertNull;
@@ -19,7 +20,9 @@ class CategoryTest {
         final var expectedIsActive = true;
 
         final Category newCategory = Category.newCategory(expectedName, expectedDescription, expectedIsActive);
+        final ThrowsValidationHandler aHandler = new ThrowsValidationHandler();
 
+        assertDoesNotThrow(() -> newCategory.validate(aHandler));
         assertNotNull(newCategory);
         assertNotNull(newCategory.getId());
         assertEquals(expectedName, newCategory.getName());
@@ -31,12 +34,133 @@ class CategoryTest {
     }
 
     @Test
+    void Given_ValidNullDescription_When_CallNewCategory_Then_InstantiateACategory() {
+        final var expectedName = "Terror";
+        final String expectedDescription = null;
+        final var expectedIsActive = true;
+
+        final Category newCategory = Category.newCategory(expectedName, expectedDescription, expectedIsActive);
+
+        final ThrowsValidationHandler aHandler = new ThrowsValidationHandler();
+
+        assertDoesNotThrow(() -> newCategory.validate(aHandler));
+        assertNotNull(newCategory);
+        assertNotNull(newCategory.getId());
+        assertEquals(expectedName, newCategory.getName());
+        assertEquals(expectedDescription, newCategory.getDescription());
+        assertEquals(expectedIsActive, newCategory.isActive());
+        assertNotNull(newCategory.getCreatedAt());
+        assertNotNull(newCategory.getUpdatedAt());
+        assertNull(newCategory.getDeletedAt());
+    }
+
+    @Test
+    void Given_ValidEmptyDescription_When_CallNewCategory_Then_InstantiateACategory() {
+        final var expectedName = "Terror";
+        final var expectedDescription = " ";
+        final var expectedIsActive = true;
+
+        final Category newCategory = Category.newCategory(expectedName, expectedDescription, expectedIsActive);
+
+        final ThrowsValidationHandler aHandler = new ThrowsValidationHandler();
+
+        assertDoesNotThrow(() -> newCategory.validate(aHandler));
+        assertNotNull(newCategory);
+        assertNotNull(newCategory.getId());
+        assertEquals(expectedName, newCategory.getName());
+        assertEquals(expectedDescription, newCategory.getDescription());
+        assertEquals(expectedIsActive, newCategory.isActive());
+        assertNotNull(newCategory.getCreatedAt());
+        assertNotNull(newCategory.getUpdatedAt());
+        assertNull(newCategory.getDeletedAt());
+    }
+
+    @Test
+    void Given_ValidFalseIsActive_When_CallNewCategory_Then_InstantiateACategory() {
+        final var expectedName = "Terror";
+        final var expectedDescription = "Contos de terror";
+        final var expectedIsActive = false;
+
+        final Category newCategory = Category.newCategory(expectedName, expectedDescription, expectedIsActive);
+
+        final ThrowsValidationHandler aHandler = new ThrowsValidationHandler();
+
+        assertDoesNotThrow(() -> newCategory.validate(aHandler));
+        assertNotNull(newCategory);
+        assertNotNull(newCategory.getId());
+        assertEquals(expectedName, newCategory.getName());
+        assertEquals(expectedDescription, newCategory.getDescription());
+        assertEquals(expectedIsActive, newCategory.isActive());
+        assertNotNull(newCategory.getCreatedAt());
+        assertNotNull(newCategory.getUpdatedAt());
+        assertNotNull(newCategory.getDeletedAt());
+    }
+
+    @Test
     void Given_AnInvalidNullName_When_CallNewCategoryAndValidate_Then_ShouldReceiveError() {
         final String expectedName = null;
         final var expectedDescription = "Contos de terror";
         final var expectedIsActive = true;
         final var expectedErrorCount = 1;
         final var expectedErrorMessage = "'name' should not be null";
+
+        final Category newCategory = Category.newCategory(expectedName, expectedDescription, expectedIsActive);
+
+        final ThrowsValidationHandler aHandler = new ThrowsValidationHandler();
+
+        final var exception = assertThrows(DomainException.class, () -> newCategory.validate(aHandler));
+
+        assertEquals(expectedErrorCount, exception.getErrors().size());
+        assertEquals(expectedErrorMessage, exception.getErrors().get(0).message());
+    }
+
+    @Test
+    void Given_AnInvalidEmptyName_When_CallNewCategoryAndValidate_Then_ShouldReceiveError() {
+        final var expectedName = " ";
+        final var expectedDescription = "Contos de terror";
+        final var expectedIsActive = true;
+        final var expectedErrorCount = 1;
+        final var expectedErrorMessage = "'name' should not be empty";
+
+        final Category newCategory = Category.newCategory(expectedName, expectedDescription, expectedIsActive);
+
+        final ThrowsValidationHandler aHandler = new ThrowsValidationHandler();
+
+        final var exception = assertThrows(DomainException.class, () -> newCategory.validate(aHandler));
+
+        assertEquals(expectedErrorCount, exception.getErrors().size());
+        assertEquals(expectedErrorMessage, exception.getErrors().get(0).message());
+    }
+
+    @Test
+    void Given_AnInvalidNameLengthLessThan3_When_CallNewCategoryAndValidate_Then_ShouldReceiveError() {
+        final var expectedName = "Fi ";
+        final var expectedDescription = "Contos de terror";
+        final var expectedIsActive = true;
+        final var expectedErrorCount = 1;
+        final var expectedErrorMessage = "'name' must be between 3 and 255 characteres";
+
+        final Category newCategory = Category.newCategory(expectedName, expectedDescription, expectedIsActive);
+
+        final ThrowsValidationHandler aHandler = new ThrowsValidationHandler();
+
+        final var exception = assertThrows(DomainException.class, () -> newCategory.validate(aHandler));
+
+        assertEquals(expectedErrorCount, exception.getErrors().size());
+        assertEquals(expectedErrorMessage, exception.getErrors().get(0).message());
+    }
+
+    @Test
+    void Given_AnInvalidNameLengthMoreThan255_When_CallNewCategoryAndValidate_Then_ShouldReceiveError() {
+        final var expectedName = """
+                    A prática cotidiana prova que a complexidade dos estudos efetuados facilita a criação das regras de conduta normativas.
+                    Nunca é demais lembrar o peso e o significado destes problemas, uma vez que a hegemonia do ambiente político desafia a
+                    capacidade de equalização de todos os recursos funcionais envolvidos.
+                """;
+        final var expectedDescription = "Contos de terror";
+        final var expectedIsActive = true;
+        final var expectedErrorCount = 1;
+        final var expectedErrorMessage = "'name' must be between 3 and 255 characteres";
 
         final Category newCategory = Category.newCategory(expectedName, expectedDescription, expectedIsActive);
 
