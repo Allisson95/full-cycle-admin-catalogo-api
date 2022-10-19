0faee80a3a652d7ad196387c621ac6f780f387d2
Allisson Nunes <allissonabn@gmail.com>
2022-10-19 15:24:34 -0300

    adicionado springboot na camada de infrastructure

---
+++ infrastructure/src/main/java/com/github/allisson95/codeflix/infrastructure/configuration/WebServerConfig.java
@@ -0,0 +1,10 @@
+package com.github.allisson95.codeflix.infrastructure.configuration;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.github.allisson95.codeflix")
+public class WebServerConfig {
+    
+}
