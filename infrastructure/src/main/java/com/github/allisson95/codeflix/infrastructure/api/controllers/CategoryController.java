06c857abd5218c0b6ed780a0d2fd2b5686a7a230
Allisson Nunes <allissonabn@gmail.com>
2022-11-01 23:04:14 -0300

    criado controller de categoria

---
+++ infrastructure/src/main/java/com/github/allisson95/codeflix/infrastructure/api/controllers/CategoryController.java
@@ -0,0 +1,22 @@
+package com.github.allisson95.codeflix.infrastructure.api.controllers;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.github.allisson95.codeflix.domain.pagination.Pagination;
+import com.github.allisson95.codeflix.infrastructure.api.CategoryAPI;
+
+@RestController
+public class CategoryController implements CategoryAPI {
+
+    @Override
+    public ResponseEntity<?> createCategory() {
+        return null;
+    }
+
+    @Override
+    public Pagination<?> listCategories(final String search, final int page, final int perPage, final String sort, final String dir) {
+        return null;
+    }
+    
+}
