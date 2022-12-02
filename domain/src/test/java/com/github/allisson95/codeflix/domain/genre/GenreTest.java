8843f7149a6cce40d95b302d17af0d80ca059922
Allisson Nunes <allissonabn@gmail.com>
2022-12-02 00:19:17 -0300

    testes unitários da entidade de gênero

---
+++ domain/src/test/java/com/github/allisson95/codeflix/domain/genre/GenreTest.java
@@ -0,0 +1,82 @@
+package com.github.allisson95.codeflix.domain.genre;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.Test;
+
+import com.github.allisson95.codeflix.domain.exceptions.DomainException;
+import com.github.allisson95.codeflix.domain.validation.handler.ThrowsValidationHandler;
+
+class GenreTest {
+
+    @Test
+    void Given_AValidParams_When_CallNewGenre_Should_InstantiateAGenre() {
+        final var expectedName = "Ação";
+        final var expectedIsActive = true;
+        final var expectedCategoriesSize = 0;
+
+        final var actualGenre = Genre.newGenre(expectedName, expectedIsActive);
+
+        assertNotNull(actualGenre);
+        assertNotNull(actualGenre.getId());
+        assertEquals(expectedName, actualGenre.getName());
+        assertEquals(expectedIsActive, actualGenre.isActive());
+        assertEquals(expectedCategoriesSize, actualGenre.getCategories().size());
+        assertNotNull(actualGenre.getCreatedAt());
+        assertNotNull(actualGenre.getUpdatedAt());
+        assertNull(actualGenre.getDeletedAt());
+    }
+
+    @Test
+    void Given_InvalidNullName_When_CallNewGenreAndValidate_Should_ReceiveAnError() {
+        final String expectedName = null;
+        final var expectedIsActive = true;
+        final var expectedErrorCount = 1;
+        final var expectedErrorMessage = "'name' should not be null";
+
+        final var actualException = assertThrows(
+                NotificationException.class,
+                () -> Genre.newGenre(expectedName, expectedIsActive));
+
+        assertEquals(expectedErrorCount, actualException.getErrors().size());
+        assertEquals(expectedErrorMessage, actualException.getErrors().get(0).message());
+    }
+
+    @Test
+    void Given_InvalidEmptyName_When_CallNewGenreAndValidate_Should_ReceiveAnError() {
+        final var expectedName = "";
+        final var expectedIsActive = true;
+        final var expectedErrorCount = 1;
+        final var expectedErrorMessage = "'name' should not be empty";
+
+        final var actualException = assertThrows(
+                NotificationException.class,
+                () -> Genre.newGenre(expectedName, expectedIsActive));
+
+        assertEquals(expectedErrorCount, actualException.getErrors().size());
+        assertEquals(expectedErrorMessage, actualException.getErrors().get(0).message());
+    }
+
+    @Test
+    void Given_InvalidNameWithLengthGreaterThan255_When_CallNewGenreAndValidate_Should_ReceiveAnError() {
+        final var expectedName = """
+                    A prática cotidiana prova que a complexidade dos estudos efetuados facilita a criação das regras de conduta normativas.
+                    Nunca é demais lembrar o peso e o significado destes problemas, uma vez que a hegemonia do ambiente político desafia a
+                    capacidade de equalização de todos os recursos funcionais envolvidos.
+                """;
+        final var expectedIsActive = true;
+        final var expectedErrorCount = 1;
+        final var expectedErrorMessage = "'name' must be between 1 and 255 characteres";
+
+        final var actualException = assertThrows(
+                NotificationException.class,
+                () -> Genre.newGenre(expectedName, expectedIsActive));
+
+        assertEquals(expectedErrorCount, actualException.getErrors().size());
+        assertEquals(expectedErrorMessage, actualException.getErrors().get(0).message());
+    }
+
+}
