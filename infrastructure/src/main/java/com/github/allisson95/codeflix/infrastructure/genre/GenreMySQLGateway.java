0f472307d784f319b5faf5649c0d84a681870783
Allisson Nunes <allissonabn@gmail.com>
2023-02-15 12:30:31 -0300

    implementado o método create do gateway de gênero

--- infrastructure/src/main/java/com/github/allisson95/codeflix/infrastructure/genre/GenreMySQLGateway.java
+++ infrastructure/src/main/java/com/github/allisson95/codeflix/infrastructure/genre/GenreMySQLGateway.java
@@ -1,5 +1,6 @@
 package com.github.allisson95.codeflix.infrastructure.genre;
 
+import java.util.Objects;
 import java.util.Optional;
 
 import org.springframework.stereotype.Component;
@@ -9,14 +10,23 @@ import com.github.allisson95.codeflix.domain.genre.GenreGateway;
 import com.github.allisson95.codeflix.domain.genre.GenreID;
 import com.github.allisson95.codeflix.domain.pagination.Pagination;
 import com.github.allisson95.codeflix.domain.pagination.SearchQuery;
+import com.github.allisson95.codeflix.infrastructure.genre.persistence.GenreJpaEntity;
+import com.github.allisson95.codeflix.infrastructure.genre.persistence.GenreRepository;
 
 @Component
 public class GenreMySQLGateway implements GenreGateway {
 
+    private final GenreRepository genreRepository;
+
+    public GenreMySQLGateway(final GenreRepository genreRepository) {
+        this.genreRepository = Objects.requireNonNull(genreRepository);
+    }
+
     @Override
-    public Genre create(Genre aGenre) {
-        // TODO Auto-generated method stub
-        return null;
+    public Genre create(final Genre aGenre) {
+        return this.genreRepository
+                .save(GenreJpaEntity.from(aGenre))
+                .toAggregate();
     }
 
     @Override
