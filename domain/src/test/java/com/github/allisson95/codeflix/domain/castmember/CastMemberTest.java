c60e4d300aa911b909d8da03c9ae525548832705
Allisson Nunes <allissonabn@gmail.com>
2023-05-21 21:57:54 -0300

    tdd das validações de castmember

--- domain/src/test/java/com/github/allisson95/codeflix/domain/castmember/CastMemberTest.java
+++ domain/src/test/java/com/github/allisson95/codeflix/domain/castmember/CastMemberTest.java
@@ -2,8 +2,11 @@ package com.github.allisson95.codeflix.domain.castmember;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.Test;
+
+import com.github.allisson95.codeflix.domain.exceptions.NotificationException;
 
 class CastMemberTest {
 
@@ -21,6 +24,74 @@ class CastMemberTest {
         assertNotNull(actualMember.getCreatedAt());
         assertNotNull(actualMember.getUpdatedAt());
         assertEquals(actualMember.getCreatedAt(), actualMember.getUpdatedAt());
+    }
+
+    @Test
+    void Given_InvalidNullName_When_CallsNewMember_Should_ReceiveANotification() {
+        final String expectedName = null;
+        final var expectedType = CastMemberType.ACTOR;
+        final var expectedErrorCount = 1;
+        final var expectedErrorMessage = "'name' should not be null";
+
+        final var actualException = assertThrows(
+                NotificationException.class,
+                () -> CastMember.newMember(expectedName, expectedType));
+
+        assertNotNull(actualException);
+        assertEquals(expectedErrorCount, actualException.getErrors().size());
+        assertEquals(expectedErrorMessage, actualException.getErrors().get(0).message());
+    }
+
+    @Test
+    void Given_InvalidEmptyName_When_CallsNewMember_Should_ReceiveANotification() {
+        final var expectedName = " ";
+        final var expectedType = CastMemberType.ACTOR;
+        final var expectedErrorCount = 1;
+        final var expectedErrorMessage = "'name' should not be empty";
+
+        final var actualException = assertThrows(
+                NotificationException.class,
+                () -> CastMember.newMember(expectedName, expectedType));
+
+        assertNotNull(actualException);
+        assertEquals(expectedErrorCount, actualException.getErrors().size());
+        assertEquals(expectedErrorMessage, actualException.getErrors().get(0).message());
+    }
+
+    @Test
+    void Given_InvalidNameWithLengthMoreThan255_When_CallsNewMember_Should_ReceiveANotification() {
+        final var expectedName = """
+                    A prática cotidiana prova que a complexidade dos estudos efetuados facilita a criação das regras de conduta normativas.
+                    Nunca é demais lembrar o peso e o significado destes problemas, uma vez que a hegemonia do ambiente político desafia a
+                    capacidade de equalização de todos os recursos funcionais envolvidos.
+                """;
+        final var expectedType = CastMemberType.ACTOR;
+        final var expectedErrorCount = 1;
+        final var expectedErrorMessage = "'name' must be between 3 and 255 characteres";
+
+        final var actualException = assertThrows(
+                NotificationException.class,
+                () -> CastMember.newMember(expectedName, expectedType));
+
+        assertNotNull(actualException);
+        assertEquals(expectedErrorCount, actualException.getErrors().size());
+        assertEquals(expectedErrorMessage, actualException.getErrors().get(0).message());
+    }
+
+    @Test
+    void Given_InvalidType_When_CallsNewMember_Should_ReceiveANotification() {
+        final var expectedName = "Vin Diesel";
+        final CastMemberType expectedType = null;
+        final var expectedErrorCount = 1;
+        final var expectedErrorMessage = "'type' should not be null";
+
+        final var actualException = assertThrows(
+                NotificationException.class,
+                () -> CastMember.newMember(expectedName, expectedType));
+
+        assertNotNull(actualException);
+        assertEquals(expectedErrorCount, actualException.getErrors().size());
+        assertEquals(expectedErrorMessage, actualException.getErrors().get(0).message());
     }
 
 }
