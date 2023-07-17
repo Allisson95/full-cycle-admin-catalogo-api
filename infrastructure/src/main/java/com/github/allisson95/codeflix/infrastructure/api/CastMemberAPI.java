9fd51226f578dd7142ebd36b31b48eb9a915ea8e
Allisson Nunes <allissonabn@gmail.com>
2023-07-17 19:08:57 -0300

    criado controller de castmember

---
+++ infrastructure/src/main/java/com/github/allisson95/codeflix/infrastructure/api/CastMemberAPI.java
@@ -0,0 +1,30 @@
+package com.github.allisson95.codeflix.infrastructure.api;
+
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
+@RequestMapping(value = "cast_members")
+@Tag(name = "Cast Members")
+public interface CastMemberAPI {
+
+    @PostMapping(
+        consumes = { MediaType.APPLICATION_JSON_VALUE },
+        produces = { MediaType.APPLICATION_JSON_VALUE }
+    )
+    @Operation(summary = "Create a new cast member")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "201", description = "Created successfully"),
+        @ApiResponse(responseCode = "422", description = "A validation error was thrown"),
+        @ApiResponse(responseCode = "500", description = "An internal server error was thrown"),
+    })
+    ResponseEntity<?> create(@RequestBody Object input);
+
+}
---
+++ infrastructure/src/main/java/com/github/allisson95/codeflix/infrastructure/api/controllers/CastMemberController.java
@@ -0,0 +1,15 @@
+package com.github.allisson95.codeflix.infrastructure.api.controllers;
+
+import org.springframework.http.ResponseEntity;
+
+import com.github.allisson95.codeflix.infrastructure.api.CastMemberAPI;
+
+public class CastMemberController implements CastMemberAPI {
+
+    @Override
+    public ResponseEntity<?> create(Object input) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'create'");
+    }
+
+}
