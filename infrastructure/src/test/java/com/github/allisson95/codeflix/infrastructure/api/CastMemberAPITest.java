fdaf813d7ad4bcab1e126638caa90c560b5e7753
Allisson Nunes <allissonabn@gmail.com>
2023-08-20 19:04:50 -0300

    testes integrados do endpoint de get de castmember por id

--- infrastructure/src/test/java/com/github/allisson95/codeflix/infrastructure/api/CastMemberAPITest.java
+++ infrastructure/src/test/java/com/github/allisson95/codeflix/infrastructure/api/CastMemberAPITest.java
@@ -6,6 +6,7 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.argThat;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
@@ -27,10 +28,13 @@ import com.github.allisson95.codeflix.Fixture;
 import com.github.allisson95.codeflix.application.castmember.create.CreateCastMemberOutput;
 import com.github.allisson95.codeflix.application.castmember.create.DefaultCreateCastMemberUseCase;
 import com.github.allisson95.codeflix.application.castmember.delete.DefaultDeleteCastMemberUseCase;
+import com.github.allisson95.codeflix.application.castmember.retrieve.get.CastMemberOutput;
 import com.github.allisson95.codeflix.application.castmember.retrieve.get.DefaultGetCastMemberByIdUseCase;
 import com.github.allisson95.codeflix.application.castmember.retrieve.list.DefaultListCastMembersUseCase;
 import com.github.allisson95.codeflix.application.castmember.update.DefaultUpdateCastMemberUseCase;
+import com.github.allisson95.codeflix.domain.castmember.CastMember;
 import com.github.allisson95.codeflix.domain.castmember.CastMemberID;
+import com.github.allisson95.codeflix.domain.exceptions.NotFoundException;
 import com.github.allisson95.codeflix.domain.exceptions.NotificationException;
 import com.github.allisson95.codeflix.domain.validation.Error;
 import com.github.allisson95.codeflix.infrastructure.castmember.models.CreateCastMemberRequest;
@@ -119,6 +123,61 @@ class CastMemberAPITest {
         verify(createCastMemberUseCase).execute(
                 argThat(cmd -> Objects.equals(expectedName, cmd.name())
                         && Objects.equals(expectedType, cmd.type())));
+    }
+
+    @Test
+    void Given_AValidId_When_CallsGetById_Should_ReturnIt() throws Exception {
+        final var expectedName = Fixture.name();
+        final var expectedType = Fixture.CastMember.type();
+
+        final var aMember = CastMember.newMember(expectedName, expectedType);
+        final var expectedId = aMember.getId();
+        final var expectedMember = CastMemberOutput.with(aMember);
+
+        when(getCastMemberByIdUseCase.execute(any()))
+                .thenReturn(expectedMember);
+
+        final var request = get("/cast_members/{id}", expectedId.getValue())
+                .accept(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.APPLICATION_JSON);
+
+        this.mvc.perform(request)
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.id", equalTo(expectedId.getValue())))
+                .andExpect(jsonPath("$.name", equalTo(expectedMember.name())))
+                .andExpect(jsonPath("$.type", equalTo(expectedMember.type().name())))
+                .andExpect(jsonPath("$.created_at", equalTo(expectedMember.createdAt().toString())))
+                .andExpect(jsonPath("$.updated_at", equalTo(expectedMember.updatedAt().toString())));
+
+        verify(getCastMemberByIdUseCase).execute(expectedId.getValue());
+    }
+
+    @Test
+    void Given_AInvalidId_When_CallsGetById_Should_ReturnNotificationException() throws Exception {
+        final var expectedId = CastMemberID.from("123");
+        final var expectedErrorMessage = "CastMember with id 123 was not found";
+
+        when(getCastMemberByIdUseCase.execute(any()))
+                .thenThrow(
+                        NotFoundException.with(CastMember.class, expectedId));
+
+        final var request = get("/cast_members/{id}", expectedId.getValue())
+                .accept(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.APPLICATION_JSON);
+
+        this.mvc.perform(request)
+                .andDo(print())
+                .andExpect(status().isNotFound())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.message").exists())
+                .andExpect(jsonPath("$.message", equalTo(expectedErrorMessage)))
+                .andExpect(jsonPath("$.errors").exists())
+                .andExpect(jsonPath("$.errors").isArray())
+                .andExpect(jsonPath("$.errors").isEmpty());
+
+        verify(getCastMemberByIdUseCase).execute(expectedId.getValue());
     }
 
 }
