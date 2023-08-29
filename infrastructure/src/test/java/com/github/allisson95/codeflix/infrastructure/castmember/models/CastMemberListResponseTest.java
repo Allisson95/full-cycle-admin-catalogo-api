29d0546bc0568b50f5fef82948621f5f06f0d98d
Allisson Nunes <allissonabn@gmail.com>
2023-08-28 23:48:39 -0300

    testes de serialização da classe castmemberlistresponse

---
+++ infrastructure/src/test/java/com/github/allisson95/codeflix/infrastructure/castmember/models/CastMemberListResponseTest.java
@@ -0,0 +1,41 @@
+package com.github.allisson95.codeflix.infrastructure.castmember.models;
+
+import java.time.Instant;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.json.JacksonTester;
+
+import com.github.allisson95.codeflix.Fixture;
+import com.github.allisson95.codeflix.JacksonTest;
+
+@JacksonTest
+class CastMemberListResponseTest {
+
+    @Autowired
+    private JacksonTester<CastMemberListResponse> json;
+
+    @Test
+    void testMarshall() throws Exception {
+        final var expectedId = "123";
+        final var expectedName = Fixture.name();
+        final var expectedType = Fixture.CastMember.type();
+        final var expectedCreatedAt = Instant.now();
+
+        final var response = new CastMemberListResponse(
+                expectedId,
+                expectedName,
+                expectedType,
+                expectedCreatedAt);
+
+        final var actualJson = this.json.write(response);
+
+        Assertions.assertThat(actualJson)
+                .hasJsonPathValue("$.id", expectedId)
+                .hasJsonPathValue("$.name", expectedName)
+                .hasJsonPathValue("$.type", expectedType.name())
+                .hasJsonPathValue("$.created_at", expectedCreatedAt.toString());
+    }
+
+}
