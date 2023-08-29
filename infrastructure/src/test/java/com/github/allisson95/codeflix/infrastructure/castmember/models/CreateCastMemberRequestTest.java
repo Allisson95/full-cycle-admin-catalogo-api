a2a84c35915c6273bae4f769cad56c997398ace9
Allisson Nunes <allissonabn@gmail.com>
2023-08-28 23:51:08 -0300

    testes de desserialização da classe createcastmemberrequest

---
+++ infrastructure/src/test/java/com/github/allisson95/codeflix/infrastructure/castmember/models/CreateCastMemberRequestTest.java
@@ -0,0 +1,36 @@
+package com.github.allisson95.codeflix.infrastructure.castmember.models;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.json.JacksonTester;
+
+import com.github.allisson95.codeflix.Fixture;
+import com.github.allisson95.codeflix.JacksonTest;
+
+@JacksonTest
+class CreateCastMemberRequestTest {
+
+    @Autowired
+    private JacksonTester<CreateCastMemberRequest> json;
+
+    @Test
+    void testUnmarshall() throws Exception {
+        final var expectedName = Fixture.name();
+        final var expectedType = Fixture.CastMember.type();
+
+        final var json = """
+                {
+                  "name": "%s",
+                  "type": "%s"
+                }
+                """.formatted(expectedName, expectedType);
+
+        final var actualJson = this.json.parse(json);
+
+        Assertions.assertThat(actualJson)
+                .hasFieldOrPropertyWithValue("name", expectedName)
+                .hasFieldOrPropertyWithValue("type", expectedType);
+    }
+
+}
