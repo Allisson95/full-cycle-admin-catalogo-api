f5ba6c171c2df16fbf7464a4b7cdc7092371d8fc
Allisson Alves Batista Nunes <allissonabn@gmail.com>
2024-03-03 15:00:31 -0300

    testes unitários do caso de uso de listagem de vídeo

---
+++ application/src/test/java/com/github/allisson95/codeflix/application/video/retrieve/list/ListVideoUseCaseTest.java
@@ -0,0 +1,131 @@
+package com.github.allisson95.codeflix.application.video.retrieve.list;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+
+import com.github.allisson95.codeflix.application.Fixture;
+import com.github.allisson95.codeflix.application.UseCaseTest;
+import com.github.allisson95.codeflix.domain.pagination.Pagination;
+import com.github.allisson95.codeflix.domain.video.Video;
+import com.github.allisson95.codeflix.domain.video.VideoGateway;
+import com.github.allisson95.codeflix.domain.video.VideoSearchQuery;
+
+class ListVideoUseCaseTest extends UseCaseTest {
+
+    @Mock
+    private VideoGateway videoGateway;
+
+    @InjectMocks
+    private DefaultListVideosUseCase useCase;
+
+    @Override
+    protected List<Object> getMocksForClean() {
+        return List.of(videoGateway);
+    }
+
+    @Test
+    void Given_AValidQuery_When_CallsListVideos_Should_ReturnVideos() {
+        final var videos = List.of(
+            Fixture.Videos.random(),
+            Fixture.Videos.random()
+        );
+
+        final var expectedPage = 0;
+        final var expectedPerPage = 10;
+        final var expectedTerms = "A";
+        final var expectedSort = "createdAt";
+        final var expectedDirection = "asc";
+        final var expectedTotal = 2;
+
+        final var expectedPagination = new Pagination<>(
+                expectedPage,
+                expectedPerPage,
+                expectedTotal,
+                videos);
+
+        final var expectedItems = videos.stream()
+                .map(VideoListOutput::from)
+                .toList();
+
+        final var aQuery = new VideoSearchQuery(expectedPage, expectedPerPage, expectedTerms, expectedSort, expectedDirection);
+
+        when(videoGateway.findAll(aQuery))
+                .thenReturn(expectedPagination);
+
+        final var actualOutput = useCase.execute(aQuery);
+
+        assertEquals(expectedPage, actualOutput.currentPage());
+        assertEquals(expectedPerPage, actualOutput.perPage());
+        assertEquals(expectedTotal, actualOutput.total());
+        assertEquals(expectedItems, actualOutput.items());
+
+        verify(videoGateway, times(1)).findAll(aQuery);
+    }
+
+    @Test
+    void Given_AValidQuery_When_CallsListVideosAndResultIsEmpty_Should_ReturnEmpty() {
+        final var videos = List.<Video>of();
+
+        final var expectedPage = 0;
+        final var expectedPerPage = 10;
+        final var expectedTerms = "A";
+        final var expectedSort = "createdAt";
+        final var expectedDirection = "asc";
+        final var expectedTotal = 0;
+
+        final var expectedPagination = new Pagination<>(
+                expectedPage,
+                expectedPerPage,
+                expectedTotal,
+                videos);
+
+        final var expectedItems = videos.stream()
+                .map(VideoListOutput::from)
+                .toList();
+
+        final var aQuery = new VideoSearchQuery(expectedPage, expectedPerPage, expectedTerms, expectedSort, expectedDirection);
+
+        when(videoGateway.findAll(aQuery))
+                .thenReturn(expectedPagination);
+
+        final var actualOutput = useCase.execute(aQuery);
+
+        assertEquals(expectedPage, actualOutput.currentPage());
+        assertEquals(expectedPerPage, actualOutput.perPage());
+        assertEquals(expectedTotal, actualOutput.total());
+        assertEquals(expectedItems, actualOutput.items());
+
+        verify(videoGateway, times(1)).findAll(aQuery);
+    }
+
+    @Test
+    void Given_AValidQuery_When_CallsListVideosAndGatewayThrowsException_Then_ReturnException() {
+        final var expectedPage = 0;
+        final var expectedPerPage = 10;
+        final var expectedTerms = "A";
+        final var expectedSort = "createdAt";
+        final var expectedDirection = "asc";
+
+        final var expectedErrorMessage = "Gateway error";
+
+        final var aQuery = new VideoSearchQuery(expectedPage, expectedPerPage, expectedTerms, expectedSort, expectedDirection);
+
+        when(videoGateway.findAll(any()))
+                .thenThrow(new IllegalStateException(expectedErrorMessage));
+
+        final var exception = assertThrows(IllegalStateException.class, () -> useCase.execute(aQuery));
+
+        assertEquals(expectedErrorMessage, exception.getMessage());
+    }
+
+}
