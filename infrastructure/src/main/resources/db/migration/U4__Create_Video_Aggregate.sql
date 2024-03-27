8e6b47056d0b5ff24003082cd9ee47dcb8e36fa8
Allisson Alves Batista Nunes <allissonabn@gmail.com>
2024-03-27 12:49:52 -0300

    criado as undo migrations das entidades de video

---
+++ infrastructure/src/main/resources/db/migration/U4__Create_Video_Aggregate.sql
@@ -0,0 +1,6 @@
+CREATE TABLE videos_categories;
+CREATE TABLE videos_genres;
+CREATE TABLE videos_cast_members;
+CREATE TABLE videos;
+CREATE TABLE videos_video_media;
+CREATE TABLE videos_image_media;
