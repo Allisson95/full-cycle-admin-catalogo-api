fba67e5dc7805a1d9672845ad93bb7f93f3a7817
Allisson Alves Batista Nunes <allissonabn@gmail.com>
2024-04-26 21:51:20 -0300

    implementado o endpoint de criação de video full

--- infrastructure/src/main/java/com/github/allisson95/codeflix/infrastructure/api/VideoAPI.java
+++ infrastructure/src/main/java/com/github/allisson95/codeflix/infrastructure/api/VideoAPI.java
@@ -1,11 +1,45 @@
 package com.github.allisson95.codeflix.infrastructure.api;
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import java.util.Set;
 
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 
 @RequestMapping(path = "videos")
 @Tag(name = "Video")
 public interface VideoAPI {
+
+    @PostMapping(consumes = { MediaType.MULTIPART_FORM_DATA_VALUE }, produces = { MediaType.APPLICATION_JSON_VALUE })
+    @Operation(summary = "Create a new video with all medias")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Created successfuly"),
+            @ApiResponse(responseCode = "422", description = "A validation error"),
+            @ApiResponse(responseCode = "500", description = "An internal server error"),
+    })
+    ResponseEntity<?> createFull(
+            @RequestParam(name = "title", required = false) String title,
+            @RequestParam(name = "description", required = false) String description,
+            @RequestParam(name = "year_launched", required = false) Integer yearLaunched,
+            @RequestParam(name = "duration", required = false) Double duration,
+            @RequestParam(name = "rating", required = false) String rating,
+            @RequestParam(name = "opened", required = false) Boolean opened,
+            @RequestParam(name = "published", required = false) Boolean published,
+            @RequestParam(name = "categories_id", required = false) Set<String> categoriesId,
+            @RequestParam(name = "genres_id", required = false) Set<String> genresId,
+            @RequestParam(name = "cast_members_id", required = false) Set<String> castMembersId,
+            @RequestParam(name = "banner_file", required = false) MultipartFile bannerFile,
+            @RequestParam(name = "thumb_file", required = false) MultipartFile thumbFile,
+            @RequestParam(name = "thumb_half_file", required = false) MultipartFile thumbHalfFile,
+            @RequestParam(name = "trailer_file", required = false) MultipartFile trailerFile,
+            @RequestParam(name = "video_file", required = false) MultipartFile videoFile);
 
 }
--- infrastructure/src/main/java/com/github/allisson95/codeflix/infrastructure/api/controllers/VideoController.java
+++ infrastructure/src/main/java/com/github/allisson95/codeflix/infrastructure/api/controllers/VideoController.java
@@ -1,10 +1,83 @@
 package com.github.allisson95.codeflix.infrastructure.api.controllers;
 
-import org.springframework.web.bind.annotation.RestController;
+import java.net.URI;
+import java.util.Objects;
+import java.util.Set;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.github.allisson95.codeflix.application.video.create.CreateVideoCommand;
+import com.github.allisson95.codeflix.application.video.create.CreateVideoUseCase;
+import com.github.allisson95.codeflix.domain.resource.Resource;
 import com.github.allisson95.codeflix.infrastructure.api.VideoAPI;
+import com.github.allisson95.codeflix.infrastructure.utils.HashingUtils;
 
 @RestController
 public class VideoController implements VideoAPI {
+
+    private final CreateVideoUseCase createVideoUseCase;
+
+    public VideoController(final CreateVideoUseCase createVideoUseCase) {
+        this.createVideoUseCase = Objects.requireNonNull(createVideoUseCase);
+    }
+
+    @Override
+    public ResponseEntity<?> createFull(
+            final String title,
+            final String description,
+            final Integer yearLaunched,
+            final Double duration,
+            final String rating,
+            final Boolean opened,
+            final Boolean published,
+            final Set<String> categoriesId,
+            final Set<String> genresId,
+            final Set<String> castMembersId,
+            final MultipartFile bannerFile,
+            final MultipartFile thumbFile,
+            final MultipartFile thumbHalfFile,
+            final MultipartFile trailerFile,
+            final MultipartFile videoFile) {
+        final var aCommand = CreateVideoCommand.with(
+                title,
+                description,
+                yearLaunched,
+                duration,
+                rating,
+                opened,
+                published,
+                categoriesId,
+                genresId,
+                castMembersId,
+                resourceOf(bannerFile),
+                resourceOf(thumbFile),
+                resourceOf(thumbHalfFile),
+                resourceOf(trailerFile),
+                resourceOf(videoFile));
+
+        final var output = this.createVideoUseCase.execute(aCommand);
+
+        return ResponseEntity
+                .created(URI.create("/videos/" + output.id()))
+                .body(output);
+    }
+
+    private Resource resourceOf(final MultipartFile part) {
+        if (part == null) {
+            return null;
+        }
+
+        try {
+            return Resource.of(
+                    HashingUtils.checksum(part.getBytes()),
+                    part.getBytes(),
+                    part.getContentType(),
+                    part.getOriginalFilename());
+        } catch (final Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
 
 }
